e26e1594302e16715c9d7ee41704f73289581d29
Olli <olli.a.sjoberg@gmail.com>
2024-03-18 20:00:30 +0200

    Add basic memory functions

--- src/memory.rs
+++ src/memory.rs
@@ -1,3 +1,28 @@
 pub struct Memory {
-    pub memory: [u8; 0xFFFF],
+    memory: [u8; 0xFFFF],
+}
+
+impl Memory {
+    fn read_byte(&self, address: usize) -> u8 {
+        self.memory[address]
+    }
+
+    fn write_byte(&mut self, address: usize, value: u8) {
+        self.memory[address] = value;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_that_byte_is_written_to_memory() {
+        let mut memory = Memory {
+            memory: [0; 0xFFFF],
+        };
+
+        memory.write_byte(5, 4);
+        assert_eq!(4, memory.read_byte(5));
+    }
 }
